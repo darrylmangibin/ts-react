3b0c99266e536ecf8ce3d4e4406ca90cc1076a29
Darryl Mangibin <darrylmangibin@gmail.com>
2021-03-10 11:17:59 +0800

    Getting User input ref

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
-const App = () => {
+import TodoList from './compoents/TodoList';
+import NewTodo from './compoents/NewTodo';
+
+const App: React.FC = () => {
+  const todos = [{ id: '1', text: 'Finish the course' }];
+
   return (
     <div className="App">
-
+      <NewTodo />
+      <TodoList items={todos} />
     </div>
   );
 }
